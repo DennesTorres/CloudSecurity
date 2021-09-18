7d4b2181357cea673a06887bac690d800c8e0fec
Dennes Torres <dennes@bufaloinfo.com.br>
2021-09-18 07:39:28 +0000

    Innitial Code

---
+++ Product.cs
@@ -0,0 +1,12 @@
+namespace AdvWorks
+{
+   public class Product
+   {
+       public int Id {get; set;}
+
+       public string ProductName {get; set;}
+
+       public double Price {get; set;}
+   }
+
+}
