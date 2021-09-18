63437d38391a346e6a3ae8d06fc0124ae01ab2b3
Dennes Torres <dennes@bufaloinfo.com.br>
2021-09-18 08:39:32 +0000

    code apparently completed

--- AdvWorks.cs
+++ AdvWorks.cs
@@ -3,24 +3,55 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-
+using System.Data.SqlClient;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AdvWorks
 {
     public static class AdvWorks
     {
         [Function("AdvWorks")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("AdvWorks");
             logger.LogInformation("C# HTTP trigger function processed a request.");
+          
+            var sqlConnection = Environment.GetEnvironmentVariable("conString");
+                
+            List<Product> productList=new List<Product>();
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            using (SqlConnection conn = new SqlConnection(sqlConnection))
+            {
+
+                
+
+                SqlCommand sqlCmdSupply = new SqlCommand();
+                SqlDataReader readerSupply;
+                sqlCmdSupply.CommandText = "select ProductId,Name,ListPrice from saleslt.Product";
+                sqlCmdSupply.Connection = conn;
+                conn.Open();
+                readerSupply = sqlCmdSupply.ExecuteReader();
+                while(readerSupply.Read())
+                {
+                    productList.Add(
+                        new Product() {
+                            Id=(int)readerSupply["ProductId"],
+                            ProductName=(string)readerSupply["Name"],
+                            Price=(double)readerSupply["ListPrice"]
+                        }
+                    );
+                }
+                readerSupply.Close();
+                conn.Close();
+            }
 
 
-            response.WriteString("Welcome to Azure Functions!");
+
+            var response = new ObjectResult(productList);
 
             return response;
         }
